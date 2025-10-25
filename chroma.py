4ac18a5442f083941da44b873821eb09e8e77306
Eric Taylor <erictaylor4954@gmail.com>
2025-10-25 13:54:58 -0700

    made chroma persistant

--- chroma.py
+++ chroma.py
@@ -8,12 +8,15 @@ import requests
 import json
 from datetime import datetime
 import pandas as pd
+
+import chromadb
+from chromadb.config import DEFAULT_TENANT, DEFAULT_DATABASE, Settings
  
 # Supabase configuration
 SUPABASE_URL = "https://grqhfzpwnehggpeclagk.supabase.co"
 SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdycWhmenB3bmVoZ2dwZWNsYWdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzNDI3MDksImV4cCI6MjA3NjkxODcwOX0.MrfkQ_qHYNUAT2_G-NXtYgf2KBEVWlWeyOWwe5NwuVs"
 
-row_count = 50
+row_count = 4_500_000
 empty_df = pd.DataFrame()
 
 def query_rows() -> pd.DataFrame: 
@@ -133,35 +136,46 @@ def connect_supabase() -> bool:
 
     return True
 
+
+
 def main() -> None:
-    
+       
     [print("Connected to supabase") if connect_supabase() else print("Failed to connect to supabase")]
  
     # Query first 5 rows
     df = query_rows()
     
-    chroma_client = chromadb.Client()
+    # chroma_client = chromadb.Client()
+
+    chroma_client = chromadb.PersistentClient(
+        path="chromadb",
+        settings=Settings(),
+        tenant=DEFAULT_TENANT,
+        database=DEFAULT_DATABASE,
+    )
     
     print("Adding category codes to collection...")
     
     # Clean entries but keep original index:
     category_codes = {
-        str(index): category_code for index, category_code in zip(df.index, df['category_code']) if category_code != None
+        str(index): category_code for index, category_code in enumerate(set(df['category_code'])) if category_code != None
     }
-
+    
+    # print(category_codes)
+    
     collection = chroma_client.create_collection(name="ecomerce_events")
     collection.add(
         ids=list(category_codes.keys()),
         documents=list(category_codes.values())
     )
     
-    
-    example_query = "smart_phone"
+    example_query = "smart phone"
     print(f"Example results for {example_query}:\n")
     # Random query for testing:
     results = collection.query(
         query_texts=[example_query],  # list(random.choice(list(category_codes.values()))), # Chroma will embed this
-        n_results=1 # how many results to return
+        n_results=5 # how many results to return
+        
     )
     print(results)
 
